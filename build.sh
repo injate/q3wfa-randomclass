55a556ea8fa62594866c9b014780bc64e25d43a6
injate <112776381+injate@users.noreply.github.com>
2023-09-11 17:15:16 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ export GOARCH=amd64
 for GOOS in windows linux darwin ; do
     suffix="-$GOOS"
     if [ "$GOOS" = "windows" ]; then suffix=".exe"; fi
-    go build -a -ldflags '-extldflags "-static"' -o "randomclass${suffix}"
+    go build -a -ldflags "-extldflags \"-static\"" -o "randomclass${suffix}"
 done
